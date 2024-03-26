044e6353dc7af54ca1d249172d7a8f473bf83b72
PigeonOliverS <sun1145@purdue.edu>
2024-03-26 00:37:03 -0400

    Creating UserManagement.java

--- Project/src/UserManagement.java
+++ Project/src/UserManagement.java
@@ -1,2 +1,15 @@
 public class UserManagement {
+    private User user;
+
+    public UserManagement(User user) {
+        this.user = user;
+    }
+
+    public UserManagement() {
+        this.user = null;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
