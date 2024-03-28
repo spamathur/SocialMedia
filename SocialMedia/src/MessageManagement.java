018240d3fb4620b0f1733ba54ac37fa08af6065d
Sparsh Mathur <107881186+spamathur@users.noreply.github.com>
2024-03-28 15:17:14 -0400

    Added functionality to MessageManagement.java

--- SocialMedia/src/MessageManagement.java
+++ SocialMedia/src/MessageManagement.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.io.*;
 
 // Javadoc goes here
 
@@ -7,38 +8,135 @@ public class MessageManagement implements MessageManager {
     private User sender;
     // The user receiveing the message
     private User reciever;
-    // The actual message itself
-    private String messageContent;
     // Integer to keep track of which message it is
     private int messageID;
-    // List of past messages which is constantly added as messsages add
+    // List of past 10 messages which is constantly added as messsages add
     private ArrayList<String> pastMessages;
+    // File name for all the messages
+    private File messageHistory;
+    // Object for synchronization
+    public static Object obj = new Object();
 
-    public MessageManagement(User sender, User reciever, String messageContent) {
+    public MessageManagement(User sender, User reciever) {
+        String realSender = sender.toString().toUpperCase();
+        String realReciever = reciever.toString().toUpperCase();
+
         // Add logic here to check if both users are valid users
-        this.sender = sender;
-        this.reciever = reciever;
-        this.messageContent = messageContent;
+        if ((realSender != "") && (realReciever != "")) {
+            this.sender = sender;
+            this.reciever = reciever;
+            this.pastMessages = new ArrayList<>();
+
+            /*
+             * Check if a file with the recipient and sender already exists if not first
+             * create
+             * the file. For example, a file of messages with abhi and vihaa would be called
+             * abhi_vihaa.txt. (notice that the file is named in alphabetical order).
+             */
+            if (realSender.compareTo(realReciever) == -1) {
+                messageHistory = new File(realSender + "_" + realReciever + ".txt");
+            }
+
+            else {
+                messageHistory = new File(realReciever + "_" + realSender + ".txt");
+            }
+
+            boolean exists = file.exists();
+
+            if (exists) {
+                BufferedReader reader = new BufferedReader(new FileReader(messageHistory));
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    pastMessages.add(line);
+                    messageID++;
+                }
+                reader.close();
+            }
+
+            else {
+                messageId = 0;
+            }
+        }
+
+        else {
+            throw new UserNotFoundException("Message cannot be sent: Invalid Users");
+        }
+
     }
 
-    /* Check if a file with the recipient and sender already exists if not first create
-    * the file. For example, a file of messages with abhi and vihaa would be called
-    * abhi_vihaa.txt. (notice that the file is named in alphabetical order). */
-
-    /* Format the message it in the way I showed above and make sure that the file
-    * access is synchronized. Make file access synchronized using ways we learned. */
+    /*
+     * Format the message it in the way I showed above and make sure that the file
+     * access is synchronized. Make file access synchronized using ways we learned.
+     */
     public void message(String message) {
-        
+        synchronized(obj) {
+            try {
+                PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(messageHistory)));
+                printWriter.println(message);
+                messageID++;
+                printWriter.close();
+            } catch (IOException e) {
+                e.printstacktrace();
+            }
+        }
     }
 
-    /*find the message with the corresponding messageId and delete it from the file. synchronize
-    * the file*/
-    public void deleteMessage(int messageID) {
-        
+    /*
+     * find the message with the corresponding messageId and delete it from the
+     * file. synchronize
+     * the file
+     */
+    public void deleteMessage(String message) {
+        synchronized(obj) {
+            try {
+                File tempFile = new File(sender.toString() + reciever.toString() + "temp.txt");
+
+                BufferedReader reader = new BufferedReader(new FileReader(messageHistory));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+                String line;
+
+                while((line = reader.readLine()) != null) {
+                    if(line.equals(message)) continue;
+                    writer.write(line);
+                    writer.newLine();
+                }
+                writer.close();
+                reader.close();
+                tempFile.renameTo(messageHistory);
+                messageID--;
+            } catch (IOException e) {
+                e.printstacktrace();
+            }
+        }
     }
 
-    /*get the last 10 messages and return as a String array. Make sure file access is synchronized*/
-    public String[] last10Messages() {
-        return null;
+    /*
+     * get the last 10 messages and return as a String array. Make sure file access
+     * is synchronized
+     */
+    public ArrayList<String> last10Messages() {
+        // Fix this
+        return pastMessages;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getReciever() {
+        return reciever;
+    }
+
+    public int getMessageID() {
+        return messageID;
+    }
+
+    public ArrayList<String> getPastMessages() {
+        return pastMessages;
+    }
+
+    public File getMessageHistory() {
+        return messageHistory;
     }
 }
