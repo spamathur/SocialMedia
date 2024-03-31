69577f7a2d0a160fd613b6f8518ed2226fce77f6
amultani11 <164709189+amultani11@users.noreply.github.com>
2024-03-31 19:57:33 -0400

    Update PostsManagerTests.java

--- RealSocialMedia/src/PostsManagerTests.java
+++ RealSocialMedia/src/PostsManagerTests.java
@@ -2,23 +2,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class PostsManagerTests {
-    private static final String TEST_FILENAME = "test_posts.txt";
-    private Post post;
+    private final static String TEST_FILENAME = "test_posts.txt";
 
     @Before
     public void setUp() throws Exception {
         setPrivateStaticField(PostsManager.class, "FILENAME", TEST_FILENAME);
-
-        post = PostsManager.createPost("testUser", "This is a test post");
+        PostsManager.getPostsList().clear();
     }
 
     @After
@@ -27,56 +22,37 @@ public class PostsManagerTests {
     }
 
     @Test
-    public void testCreatePost() {
-        assertNotNull("Post should not be null after creation", post);
-        assertEquals("Post content should match", "This is a test post", post.getContent());
-        assertEquals("Post creator should match", "testUser", post.getCreator());
-    }
+    public void testCreateAndFindPost() {
+        Post post = PostsManager.createPost("testUser", "Hello World");
+        assertNotNull(post);
 
-    @Test
-    public void testFindPost() {
         Post foundPost = PostsManager.findPost(post.getPostID());
-        assertEquals("Found post should match the created post", post, foundPost);
+        assertEquals("The found post should match the created post", post, foundPost);
     }
 
     @Test
-    public void testWriteAndReadPosts() throws IOException {
+    public void testGetPostsList() {
+        PostsManager.createPost("user1", "Post 1");
+        PostsManager.createPost("user2", "Post 2");
+
+        List<Post> posts = PostsManager.getPostsList();
+        assertEquals("There should be 2 posts in the list", 2, posts.size());
+    }
+
+    @Test
+    public void testReadAndWritePosts() throws IOException {
+        PostsManager.createPost("testUser", "Test post content");
         PostsManager.writePosts();
-        PostsManager.postsList.clear();
+        PostsManager.getPostsList().clear();
         PostsManager.readPosts();
-
-        Post readPost = PostsManager.findPost(post.getPostID());
-        assertNotNull("Post should be read from file", readPost);
-        assertEquals("Content of read post should match", post.getContent(), readPost.getContent());
+        List<Post> posts = PostsManager.getPostsList();
+        assertEquals("There should be 1 post after reading from the file", 1, posts.size());
+        assertEquals("The post content should match", "Test post content", posts.get(0).getContent());
     }
 
-    @Test
-    public void testConcurrency() throws InterruptedException {
-        ExecutorService service = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 100; i++) {
-            service.submit(() -> {
-                PostsManager.createPost("concurrentUser", "Concurrent post content");
-            });
-        }
-        service.shutdown();
-        service.awaitTermination(1, TimeUnit.MINUTES);
-        
-        try {
-            PostsManager.writePosts();
-        } catch (IOException e) {
-            fail("IOException should not occur");
-        }
-
-        assertTrue("There should be 101 posts after concurrent post creation (including setUp post)",
-                PostsManager.postsList.size() == 101);
-    }
-
-    private void setPrivateStaticField(Class<?> clazz, String fieldName, Object value) throws Exception {
+    private static void setPrivateStaticField(Class<?> clazz, String fieldName, Object value) throws Exception {
         java.lang.reflect.Field field = clazz.getDeclaredField(fieldName);
         field.setAccessible(true);
-        java.lang.reflect.Field modifiersField = java.lang.reflect.Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~java.lang.reflect.Modifier.FINAL);
         field.set(null, value);
     }
 }
