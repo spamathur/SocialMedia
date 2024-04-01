46796bc68e79c7e5c14bc3ccbc52b4b52b21c9c5
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-03-31 23:31:23 -0400

    Update FileManagerInterface.java
    added comments/javadoc

--- RealSocialMedia/src/FileManagerInterface.java
+++ RealSocialMedia/src/FileManagerInterface.java
@@ -1,7 +1,16 @@
 import java.io.IOException;
 
-public interface FileManagerInterface {
-    void readAll() throws IOException;
+/**
+ * Manages files for the CommentsManager, PostsManager, and UsersManager classes
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
 
-    void writeAll() throws IOException;
+public interface FileManagerInterface {
+    void readAll() throws IOException; //reads comments, posts, and users
+
+    void writeAll() throws IOException; //writes users, posts, and comments
 }
