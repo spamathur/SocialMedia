aa780c04ead2696fd56c86dc37946e160d2e3bd5
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-03-31 23:44:44 -0400

    Update CommentInterface.java

--- RealSocialMedia/src/CommentInterface.java
+++ RealSocialMedia/src/CommentInterface.java
@@ -1,31 +1,39 @@
+/**
+ * A class dedicated to creating and modifying comments
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
 public interface CommentInterface {
-    void upvote();
+    void upvote(); //increases a comment's upvote
 
-    void downvote();
+    void downvote(); //increases a comment's downvote
 
-    String getPostAuthor();
+    String getPostAuthor(); //returns the post's author
 
-    void setPostAuthor(String postAuthor);
+    void setPostAuthor(String postAuthor); //sets the post's author
 
-    String getCommentID();
+    String getCommentID(); //gets the comment's ID
 
-    void setCommentID(String commentID);
+    void setCommentID(String commentID); //sets the comment's ID
 
-    String getCreator();
+    String getCreator(); //returns the creator
 
-    void setCreator(String creator);
+    void setCreator(String creator); //sets the creator
 
-    String getContent();
+    String getContent(); //returns content
 
-    void setContent(String content);
+    void setContent(String content); //sets content
 
-    int getUpvotes();
+    int getUpvotes(); //returns the amount of upvotes
 
-    void setUpvotes(int upvotes);
+    void setUpvotes(int upvotes); //sets the amount of upvotes to a given int
 
-    int getDownvotes();
+    int getDownvotes(); //returns the downvotes
 
-    void setDownvotes(int downvotes);
+    void setDownvotes(int downvotes); //sets the amount of downvotes to a given int
 
-    String toString();
+    String toString(); //returns a comment string in the format of commentID, creator, content, postAuthor, upvotes, downvotes
 }
