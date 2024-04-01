6fbc351b12a542f25ab28a4df173d96d43845e64
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-04-01 00:12:04 -0400

    Update PostNotFoundException.java

--- RealSocialMedia/src/PostNotFoundException.java
+++ RealSocialMedia/src/PostNotFoundException.java
@@ -1,3 +1,12 @@
+/**
+ * Post not found exception
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+
 public class PostNotFoundException extends Exception {
     public PostNotFoundException(){
         super();
