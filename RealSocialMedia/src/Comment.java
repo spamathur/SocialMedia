252c646179aadfc4f847bf2dc180cf6bfce34118
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-04-01 00:08:18 -0400

    Update Comment.java

--- RealSocialMedia/src/Comment.java
+++ RealSocialMedia/src/Comment.java
@@ -1,5 +1,14 @@
 import java.util.UUID;
 
+/**
+ * A class dedicated to creating and modifying comments
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+
 public class Comment {
     private String commentID;
     private String postAuthor;
