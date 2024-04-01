66d22cb855fef925e1396a9369e549461bab4512
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-04-01 00:10:50 -0400

    Update UserNameTakenException.java

--- RealSocialMedia/src/UserNameTakenException.java
+++ RealSocialMedia/src/UserNameTakenException.java
@@ -1,3 +1,12 @@
+/**
+ * Username taken exception
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+
 public class UserNameTakenException extends Exception {
     public UserNameTakenException(){
         super();
