2bfaf6bf86d43b155e59b450d313573375af6d0d
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-03-31 23:22:44 -0400

    Update PostsManagerInterface.java

--- RealSocialMedia/src/PostsManagerInterface.java
+++ RealSocialMedia/src/PostsManagerInterface.java
@@ -1,12 +1,24 @@
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * a class dedicated to finding, reading, and saving posts
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+
 public interface PostsManagerInterface {
-    Post findPost(String postID);
 
-    Post createPost(String creator, String content);
+    void setPostsList(List<Post> postsList); //sets posts to given posts
 
-    void readPosts() throws IOException;
+    Post findPost(String postID); //given a postID, finds a post
 
-    void writePosts() throws IOException;
+    Post createPost(String creator, String content); //creates a post, adds it to existing post list
+
+    void readPosts() throws IOException; //reads a file, and gets the post information (post ID, upvote, downvote)
+
+    void writePosts() throws IOException; //writes posts (string format) to a file
 }
