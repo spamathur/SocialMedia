b1f5dd49b1aa880c098903bd42732fd7ac1c987b
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-04-01 00:07:02 -0400

    Update FileManager.java

--- RealSocialMedia/src/FileManager.java
+++ RealSocialMedia/src/FileManager.java
@@ -1,5 +1,14 @@
 import java.io.IOException;
 
+/**
+ * Manages files for the CommentsManager, PostsManager, and UsersManager classes
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+
 public class FileManager {
     public static void readAll() throws IOException {
         CommentsManager.readComments();
