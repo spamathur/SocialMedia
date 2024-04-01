0168d3330ac587eb32cb131462976d25535cf318
PigeonOliverS <156121129+PigeonOliverS@users.noreply.github.com>
2024-04-01 00:25:00 -0400

    Update CommentsManagerInterface.java
    static

--- RealSocialMedia/src/CommentsManagerInterface.java
+++ RealSocialMedia/src/CommentsManagerInterface.java
@@ -2,25 +2,34 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
-/**
- * An interface dedicated to modifying, finding, and saving comments
- * <p>
- * Purdue University -- CS18000 -- Spring 2024
- *
- * @author Project 5 Team 3 Lab 27
- * @version March 31, 2024
- */
-
 public interface CommentsManagerInterface {
-    List<Comment> getCommentsList(); //returns comments
+    static List<Comment> getCommentsList() //returns comments
+    {
+        return null;
+    }
 
-    void deleteComment(String commentID); //finds and deletes a comment based on its ID
+    static void deleteComment(String commentID) //finds and deletes a comment based on its ID
+    {
 
-    void createComment(String postID, String creator, String content); //creates a comment
+    }
 
-    Comment findComment(String commentID); //finds a comment with its ID
+    static void createComment(String postID, String creator, String content) //creates a comment
+    {
 
-    void readComments() throws IOException; //reads a file and gets a comment with its ID, upvotes, downvotes
+    }
 
-    void writeComments() throws IOException; //writes all comments to a file
+    static Comment findComment(String commentID) //finds a comment with its ID
+    {
+        return null;
+    }
+
+    static void readComments() throws IOException //reads a file and gets a comment with its ID, upvotes, downvotes
+    {
+
+    }
+
+    static void writeComments() throws IOException //writes all comments to a file
+    {
+
+    }
 }
