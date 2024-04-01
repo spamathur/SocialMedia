aeef2f94dbd0cdddd3ae89eb9642dd78117ed072
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-04-01 00:12:34 -0400

    Update LogInFailedException.java

--- RealSocialMedia/src/LogInFailedException.java
+++ RealSocialMedia/src/LogInFailedException.java
@@ -1,3 +1,12 @@
+/**
+ * Login failed exception
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+
 public class LogInFailedException extends Exception {
     public LogInFailedException(){
         super();
