ec40520086d926051a427abfee4362ed0df0c8e9
Sparsh Mathur <107881186+spamathur@users.noreply.github.com>
2024-04-01 01:13:52 -0400

    Deleted SocialMedia directory

--- SocialMedia/src/LoginFailedException.java
+++
@@ -1,5 +0,0 @@
-public class LoginFailedException extends Exception {
-    public LoginFailedException(String message){
-        super(message);
-    }
-}
--- SocialMedia/src/UsernameTakenException.java
+++
@@ -1,5 +0,0 @@
-public class UsernameTakenException extends Exception {
-    public UsernameTakenException(String message){
-        super(message);
-    }
-}
