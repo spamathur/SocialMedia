79599d8ba9fbe0a2b5043d07c50e2eca9ed37e1b
Sparsh Mathur <107881186+spamathur@users.noreply.github.com>
2024-04-01 01:27:32 -0400

    Updated UserTest.java

--- RealSocialMedia/src/UserTest.java
+++ RealSocialMedia/src/UserTest.java
@@ -1,101 +1,155 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
+/**
+ * A framework to run public test cases for User.java for PJ5.
+ *
+ * @author Project 5 Team 3 Lab 27
+ *
+ * @version March 31, 2024
+ */
+
 public class UserTest {
-    private User user;
 
-    @Before
-    public void setUp() throws UserNameTakenException {
-        // Add a new user for testing
-        user = new User("john", "John", "Doe", "password", "profile.jpg");
-        UsersManager.signUp(user);
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(UserTest.TestCase.class);
+        if (result.wasSuccessful()) {
+            System.out.println("Excellent - Test ran successfully");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
     }
 
-    @Test
-    public void testSearchUsers() throws UserNameTakenException {
-        User newUser = new User("jane", "Jane", "Doe", "password", "profile.jpg");
-        UsersManager.signUp(newUser);
-        assertEquals(1, user.searchUsers("Jane").size());
-    }
+    public static class TestCase {
+        // User object to be tested
+        private User user;
 
-    public void testSetAndGetUserName() {
-        user.setUserName("johnny");
-        assertEquals("johnny", user.getUserName());
-    }
+        // Set up the user object
+        @Before
+        public void setUp() throws UserNameTakenException {
+            // Add a new user for testing
+            user = new User("john", "John", "Doe", "password", "profile.jpg");
+        }
 
-    @Test
-    public void testSetAndGetFirstName() {
-        user.setFirstName("Johnny");
-        assertEquals("Johnny", user.getFirstName());
-    }
+        // Test to check the search functionality
+        @Test(timeout = 1000)
+        public void testSearchUsers() throws UserNameTakenException {
+            User newUser = new User("jane", "Jane", "Doe", "password", "profile.jpg");
+            UsersManager.signUp(newUser);
+            Assert.assertEquals(1, user.searchUsers("Jane").size());
+        }
 
-    @Test
-    public void testSetAndGetLastName() {
-        user.setLastName("Dough");
-        assertEquals("Dough", user.getLastName());
-    }
+        // Test to check the setters
+        @Test(timeout = 1000)
+        public void testSetAndGetUserName() {
+            user.setUserName("johnny");
+            Assert.assertEquals("johnny", user.getUserName());
+            user.setFirstName("Johnny");
+            Assert.assertEquals("Johnny", user.getFirstName());
+            user.setLastName("Dough");
+            Assert.assertEquals("Dough", user.getLastName());
+            user.setPassword("newpassword");
+            Assert.assertEquals("newpassword", user.getPassword());
+            user.setProfilePic("new_profile.jpg");
+            Assert.assertEquals("new_profile.jpg", user.getProfilePic());
+        }
 
-    @Test
-    public void testSetAndGetPassword() {
-        user.setPassword("newpassword");
-        assertEquals("newpassword", user.getPassword());
-    }
+        // Test to check getting friends posts
+        @Test(timeout = 1000)
+        public void testGetMyFriendsPosts() throws UserNameTakenException {
+            User friend = new User("chenbo", "Chenbo", "Doe", "password", "profile.jpg");
+            UsersManager.signUp(friend);
+            user.addFriend("chenbo");
+            friend.createPost("Testing something");
+            Assert.assertEquals(1, user.getMyFriendsPosts().size());
+        }
 
-    @Test
-    public void testSetAndGetProfilePic() {
-        user.setProfilePic("new_profile.jpg");
-        assertEquals("new_profile.jpg", user.getProfilePic());
-    }
+        // Test to check hide post
+        @Test(timeout = 1000)
+        public void testHidePost() {
+            Post post = PostsManager.createPost("john", "Hello World");
+            user.addMyPosts(post);
+            user.hidePost(post.getPostID());
+            Assert.assertTrue(user.getMyPosts().contains(post));
+            User friend = new User("jane", "Jane", "Doe", "password", "profile.jpg");
+            user.addFriend("jane");
+            Assert.assertFalse(friend.getMyFriendsPosts().contains(post));
+            Assert.assertTrue(user.getHiddenPosts().contains(post));
+        }
 
-    @Test
-    public void testGetMyFriendsPosts() {
-        User friend = new User("jane", "Jane", "Doe", "password", "profile.jpg");
-        Post post = new Post("jane", "Hello from Jane");
-        friend.addMyPosts(post);
-        user.addFriend("jane");
-        UsersManager.getUsersList().add(friend);
-        assertEquals(1, user.getMyFriendsPosts().size());
-        assertEquals(post, user.getMyFriendsPosts().get(0));
-    }
+        // Test to check creating a post
+        @Test(timeout = 1000)
+        public void testCreatePost() {
+            user.setMyPosts(new ArrayList<>());
+            user.createPost("Hello World 2");
+            Assert.assertEquals(1, user.getMyPosts().size());
+            Assert.assertEquals("Hello World 2", user.getMyPosts().get(0).getContent());
+        }
 
-    @Test
-    public void testHidePost() {
-        Post post = PostsManager.createPost("john", "Hello World");
-        user.addMyPosts(post);
-        user.hidePost(post.getPostID());
-        assertTrue(user.getMyPosts().contains(post));
-        assertTrue(user.getHiddenPosts().contains(post));
-    }
+        // Test to check creating a comment
+        @Test(timeout = 1000)
+        public void testCreateComment() {
+            user.setMyPosts(new ArrayList<>());
+            user.createPost("Hello World 3");
+            Post post = user.getMyPosts().get(0);
+            user.createComment(post.getPostID(), "Nice post!");
+            Assert.assertEquals(1, post.getComments().size());
+            Assert.assertEquals("Nice post!", post.getComments().get(0).getContent());
+        }
 
-    @Test
-    public void testCreatePost() {
-        user.createPost("Hello World");
-        assertEquals(1, user.getMyPosts().size());
-        assertEquals("Hello World", user.getMyPosts().get(0).getContent());
-    }
+        // Test to check upvote
+        @Test(timeout = 1000)
+        public void testUpvotePost() {
+            user.setMyPosts(new ArrayList<>());
+            Post post = PostsManager.createPost("john", "Hello World");
+            user.upvotePost(post.getPostID());
+            Assert.assertEquals(1, post.getUpvotes());
+        }
 
-    @Test
-    public void testCreateComment() {
-        Post post = PostsManager.createPost("john", "Hello World");
-        user.createComment(post.getPostID(), "Nice post!");
-        assertEquals(1, post.getComments().size());
-        assertEquals("Nice post!", post.getComments().get(0).getContent());
-    }
+        // Test to check downvote
+        @Test(timeout = 1000)
+        public void testDownvotePost() {
+            user.setMyPosts(new ArrayList<>());
+            Post post = PostsManager.createPost("john", "Hello World");
+            user.downvotePost(post.getPostID());
+            Assert.assertEquals(1, post.getDownvotes());
+        }
 
-    @Test
-    public void testUpvotePost() {
-        Post post = PostsManager.createPost("john", "Hello World");
-        user.upvotePost(post.getPostID());
-        assertEquals(1, post.getUpvotes());
-    }
+        // Test to ensure that the class is declared properly
+        @Test(timeout = 1000)
+        public void UserClassDeclarationTest() {
+            Class<?> clazz;
+            int modifiers;
+            Class<?> superclass;
+            Class<?>[] superinterfaces;
 
-    @Test
-    public void testDownvotePost() {
-        Post post = PostsManager.createPost("john", "Hello World");
-        user.downvotePost(post.getPostID());
-        assertEquals(1, post.getDownvotes());
-    }
+            clazz = User.class;
 
-    // Additional tests can be added for other methods.
+            modifiers = clazz.getModifiers();
+
+            superclass = clazz.getSuperclass();
+
+            superinterfaces = clazz.getInterfaces();
+
+            Assert.assertTrue("Ensure that `Comment` is `public`!",
+                    Modifier.isPublic(modifiers));
+            Assert.assertFalse("Ensure that `Comment` is NOT `abstract`!",
+                    Modifier.isAbstract(modifiers));
+            Assert.assertEquals("Ensure that `Comment` extends `Object`!",
+                    Object.class, superclass);
+            Assert.assertEquals("Ensure that `Comment` implements one interfaces!",
+                    1, superinterfaces.length);
+        }
+    }
 }
