a7d5fcddaf5933347c69e02791603ff42c6408b3
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-03-31 23:37:11 -0400

    Update CommentsManagerInterface.java

--- RealSocialMedia/src/CommentsManagerInterface.java
+++ RealSocialMedia/src/CommentsManagerInterface.java
@@ -2,16 +2,25 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * A class dedicated to modifying, finding, and saving comments
+ * <p>
+ * Purdue University -- CS18000 -- Spring 2024
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+
 public interface CommentsManagerInterface {
-    List<Comment> getCommentsList();
+    List<Comment> getCommentsList(); //returns comments
 
-    void deleteComment(String commentID);
+    void deleteComment(String commentID); //finds and deletes a comment based on its ID
 
-    void createComment(String postID, String creator, String content);
+    void createComment(String postID, String creator, String content); //creates a comment
 
-    Comment findComment(String commentID);
+    Comment findComment(String commentID); //finds a comment with its ID
 
-    void readComments() throws IOException;
+    void readComments() throws IOException; //reads a file and gets a comment with its ID, upvotes, downvotes
 
-    void writeComments() throws IOException;
+    void writeComments() throws IOException; //writes all comments to a file
 }
