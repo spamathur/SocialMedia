efe41bdddaf8b478897018cc757adb5126148253
Siva Durga Abhinav Padmanabhuni <padman18@purdue.edu>
2024-04-11 17:15:03 -0400

    Update Server.java

--- RealSocialMedia/src/Server.java
+++ RealSocialMedia/src/Server.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Project 5 -- Social Media
  * <p>
@@ -10,5 +14,21 @@
  * @author Project 5 Team 3 Lab 27
  * @version March 31, 2024
  */
-public interface Server {
+public class Server {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(4242)) {
+            FileManager.readAll();
+            while (true) {
+                System.out.println("Waiting to connect...");
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected...");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread clientThread = new Thread(clientHandler);
+                clientThread.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
