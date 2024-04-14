6b4c85f09cbc59eaba106e8d7b9207c6fdb42634
christinam4 <139832360+christinam4@users.noreply.github.com>
2024-04-14 14:59:59 -0400

    Add files via upload

---
+++ RealSocialMedia/src/ClientHandlerInterface.java
@@ -0,0 +1,16 @@
+/**
+ * Project 5 -- Social Media
+ * <p>
+ * This class is the thread that
+ * deals with every client. It
+ * implements runnable.
+ *
+ * @author Project 5 Team 3 Lab 27
+ * @version March 31, 2024
+ */
+public interface ClientHandlerInterface {
+
+    void run();
+    //this class performs all the user methods like create comments, hide posts, sign up, while being thread safe
+
+}
